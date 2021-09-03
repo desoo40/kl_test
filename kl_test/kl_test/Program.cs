f8d0f7048979d83b56a9b2f622e6f5e4cacda34b
Denis Sorokin <deso40@gmail.com>
2021-09-03 18:11:36 +0300

    first comm

---
+++ kl_test/kl_test/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+var kek = new List<int>();
+
+kek.Add(1);
+kek.Add(3);
+
+foreach (var el in kek)
+    Console.WriteLine(el);
