c3b82ea6980272610ab504efdd570d4fcb40c386
Denis Sorokin <deso40@gmail.com>
2021-09-03 18:43:29 +0300

    get the response

--- kl_test/kl_test/Program.cs
+++ kl_test/kl_test/Program.cs
@@ -1,10 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.IO;
+using System.Net;
 
-Console.WriteLine("Hello, World!");
-var kek = new List<int>();
+namespace kl_test // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var token = "iTlZ104fQ+WofNbYc/EiEg==";
 
-kek.Add(1);
-kek.Add(3);
+            string sURL;
+            
+            var hash = "ac90ad929d7f5d6dd5c06809ac8613c9";
 
-foreach (var el in kek)
-    Console.WriteLine(el);
+            sURL = $"https://opentip.kaspersky.com/api/v1/search/hash?request={hash}";
+
+
+
+            var wrGETURL = WebRequest.Create(sURL);
+            wrGETURL.Headers.Add("x-api-key", token);
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            int i = 0;
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    Console.WriteLine("{0}:{1}", i, sLine);
+            }
+            Console.ReadLine();
+        }
+    }
+}
