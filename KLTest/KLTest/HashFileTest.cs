13422e6d18b31aeaa0ea69f1a2f6bf456f7555d8
Denis Sorokin <deso40@gmail.com>
2021-09-07 02:01:14 +0300

    refactor code. Empty string token bug?

--- KLTest/KLTest/HashFileTest.cs
+++ KLTest/KLTest/HashFileTest.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net;
 using Newtonsoft.Json;
 using System.IO;
+using System.Collections.Generic;
 using System;
 
 namespace KLTest
@@ -9,13 +10,20 @@ namespace KLTest
     [TestClass]
     public class HashFileTest
     {
-        const string validToken = "gMJUS2SoSPaxEww8mPOY+A==";
+        const string validToken = "/t2gSwUfQvyKrfPX9v2H3w==";
         const string Ox = "0x";
         const string validHashMd5 = "AC90AD929D7F5D6DD5C06809AC8613C9";
         const string validHashSha1 = "BD8BBD7F603CF8B51097C0E416FBBE14F561A994";
         const string validHashSha256 = "B27CC938BE34A9455E567EEC0A27381A89D7C8348F8A721A6D167D34C53C4B4A";
 
         const string sURL = "https://opentip.kaspersky.com/api/v1/search/hash?request=";
+
+        const string webCatchAssertText = "Cought web exception, but not ";
+
+        const string badRequestErrCode = "400";
+        const string badRequestErrName = "Bad request";
+        const string unauthorizedtErrCode = "401";
+        const string unauthorizedtErrName = "Unauthorized";
 
         public static string WebReq(string url, string token)
         {
@@ -45,107 +53,107 @@ namespace KLTest
             }
         }
 
-        [TestMethod]
-        public void CheckCorrectHashTokenMd5()
+        public void AssertExeptionCatcher(string token, string hash, string errCode, string errMes)
         {
-            var req = WebReq(sURL + validHashMd5, validToken);
-            
             try
             {
-                var info = JsonConvert.DeserializeObject<FileInfo>(req);
-
-                Assert.IsTrue(info.AllFieldsNotNull(), "Md5 hash wrong response");
+                WebReq(sURL + hash, token);
+            }
+            catch (WebException ex)
+            {
+                Assert.IsTrue(
+                    ex.Message.Contains(errCode),
+                    $"{webCatchAssertText} {errCode}\n {errMes}"
+                );
+                return;
             }
             catch (Exception ex)
             {
                 Assert.Fail(ex.Message);
             }
+
+            Assert.Fail(errMes);
+        }
+
+        public void AssertIsTrueChecker(string token, string hash, string errMes)
+        {
+            var req = WebReq(sURL + validHashMd5, validToken);
+
+            try
+            {
+                var info = JsonConvert.DeserializeObject<FileInfo>(req);
+
+                Assert.IsTrue(info.AllFieldsNotNull(), errMes);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        public string ErrorStringBuilder(string inBegining, string errCode, string errName)
+        {
+            return $"{inBegining} doesn't give {errCode} ({errName}) error";
+        }
+
+       [TestMethod]
+        public void CheckCorrectHashTokenMd5()
+        {
+            AssertIsTrueChecker(
+                validHashMd5,
+                validToken,
+                "Md5 hash wrong response"
+            );
         }
 
         [TestMethod]
         public void CheckCorrectHashTokenMd5_0x()
         {
-            var req = WebReq(sURL + Ox + validHashMd5, validToken);
-
-            try
-            {
-                var info = JsonConvert.DeserializeObject<FileInfo>(req);
-
-                Assert.IsTrue(info.AllFieldsNotNull(), "Md5 hash with 0x prefix wrong response");
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
+            AssertIsTrueChecker(
+               Ox + validHashMd5,
+               validToken,
+               "Md5 hash with 0x prefix wrong response"
+           );
         }
 
         [TestMethod]
         public void CheckCorrectHashTokenSha1()
         {
-            var req = WebReq(sURL + validHashSha1, validToken);
-
-            try
-            {
-                var info = JsonConvert.DeserializeObject<FileInfo>(req);
-
-                Assert.IsTrue(info.AllFieldsNotNull(), "Sha1 hash wrong response");
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
+            AssertIsTrueChecker(
+               validHashSha1,
+               validToken,
+               "Sha1 hash wrong response"
+           );
         }
 
         [TestMethod]
         public void CheckCorrectHashTokenSha1_0x()
         {
-            var req = WebReq(sURL + Ox + validHashSha1, validToken);
-            
-            try
-            {
-                var info = JsonConvert.DeserializeObject<FileInfo>(req);
-
-                Assert.IsTrue(info.AllFieldsNotNull(), "Sha1 hash with 0x prefix wrong response");
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
+            AssertIsTrueChecker(
+               Ox + validHashSha1,
+               validToken,
+               "Sha1 hash with 0x prefix wrong response"
+           );
         }
 
         [TestMethod]
         public void CheckCorrectHashTokenSha256()
         {
-            var req = WebReq(sURL + validHashSha256, validToken);
-
-            try
-            {
-                var info = JsonConvert.DeserializeObject<FileInfo>(req);
-
-                Assert.IsTrue(info.AllFieldsNotNull(), "Sha256 hash wrong response");
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
+            AssertIsTrueChecker(
+               validHashSha256,
+               validToken,
+               "Sha256 hash wrong response"
+           );
         }
 
         [TestMethod]
         public void CheckCorrectHashTokenSha256_0x()
         {
-            var req = WebReq(sURL + Ox + validHashSha256, validToken);
-
-            try
-            {
-                var info = JsonConvert.DeserializeObject<FileInfo>(req);
-
-                Assert.IsTrue(info.AllFieldsNotNull(), "Sha256 hash with 0x prefix wrong response");
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
+            AssertIsTrueChecker(
+               Ox + validHashSha256,
+               validToken,
+               "Sha256 hash with 0x prefix wrong response"
+           );
         }
 
         [TestMethod]
@@ -177,19 +185,12 @@ namespace KLTest
         public void ToLowerHash()
         {
             var lowerHash = validHashMd5.ToLower();
-            var req = WebReq(sURL + lowerHash, validToken);
 
-            try
-            {
-                var info = JsonConvert.DeserializeObject<FileInfo>(req);
-
-                Assert.IsTrue(info.AllFieldsNotNull(), "Hash in lower case gives wrong response");
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
+            AssertIsTrueChecker(
+               lowerHash,
+               validToken,
+               "Hash in lower case gives wrong response"
+           );
         }
 
         [TestMethod]
@@ -211,298 +212,190 @@ namespace KLTest
         public void IncorrectLenghtHash()
         {
             var incorrectLenght = validHashMd5 + "extra";
-            var errorMes = "Hash with incorrect lenght doesn't give 400 (Bad Request) error";
 
-            try
-            {
-                WebReq(sURL + incorrectLenght, validToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("400"),
-                    $"Cought web exception, but not 400 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                  validToken,
+                  incorrectLenght,
+                  badRequestErrCode,
+                  ErrorStringBuilder(
+                      "Hash with incorrect lenght",
+                      badRequestErrCode,
+                      badRequestErrName
+                  )
+              );
         }
 
         [TestMethod]
         public void EmptyStringHash()
         {
             var emptyStringHash = "";
-            var errorMes = "Empty string hash doesn't give 400 (Bad Request) error";
 
-            try
-            {
-                WebReq(sURL + emptyStringHash, validToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("400"),
-                     $"Cought web exception, but not 400 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                  validToken,
+                  emptyStringHash,
+                  badRequestErrCode,
+                  ErrorStringBuilder(
+                      "Empty string hash",
+                      badRequestErrCode,
+                      badRequestErrName
+                  )
+              );
         }
 
         [TestMethod]
         public void EmptyFileHash()
         {
             var emptyFileHash = "D41D8CD98F00B204E9800998ECF8427E";
-            var errorMes = "Empty file hash doesn't give 400 (Bad Request) error";
 
-            try
-            {
-                WebReq(sURL + emptyFileHash, validToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("400"),
-                    $"Cought web exception, but not 400 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                 validToken,
+                 emptyFileHash,
+                 badRequestErrCode,
+                 ErrorStringBuilder(
+                     "Empty file hash",
+                     badRequestErrCode,
+                     badRequestErrName
+                 )
+             );
         }
 
         [TestMethod]
         public void UnknownHash()
         {
             var unknownHash = "229d33b2d0d7d50441feb476a88d1373";
-            var errorMes = "Unknown by app file hash doesn't give 400 (Bad Request) error";
 
-            try
-            {
-                WebReq(sURL + unknownHash, validToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("400"),
-                    $"Cought web exception, but not 400 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                validToken,
+                unknownHash,
+                badRequestErrCode,
+                ErrorStringBuilder(
+                    "Unknown by app file hash",
+                    badRequestErrCode,
+                    badRequestErrName
+                )
+            );
         }
 
         [TestMethod]
         public void IncorrectCharactersHash()
         {
             var incorrectCharsHash = "AZ90AG929D7F5D6DD5C06809AC8XXYY";
-            var errorMes = "Hash with incorrect chars doesn't give 400 (Bad Request) error";
 
-            try
-            {
-                WebReq(sURL + incorrectCharsHash, validToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("400"),
-                    $"Cought web exception, but not 400 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                validToken,
+                incorrectCharsHash,
+                badRequestErrCode,
+                ErrorStringBuilder(
+                    "Hash with incorrect chars",
+                    badRequestErrCode,
+                    badRequestErrName
+                )
+            );
         }
 
+        #region Tokens part
         [TestMethod]
         public void ExpiredToken()
         {
             var expiredToken = "9eXC2xJ8REyxkQUEm6iOXg=="; // till 5.09.2021
-            var errorMes = "Expiered token doesn't give 401 (Unautorized) error";
 
-            try
-            {
-                WebReq(sURL + validHashMd5, expiredToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("401"),
-                    $"Cought web exception, but not 401 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                expiredToken,
+                validHashMd5,
+                unauthorizedtErrCode,
+                ErrorStringBuilder(
+                    "Expiered token",
+                    unauthorizedtErrCode,
+                    unauthorizedtErrName
+                )
+            );
         }
 
         [TestMethod]
         public void RevokeToken()
         {
             var revokedToken = "iTlZ104fQ+WofNbYc/EiEg==";
-            var errorMes = "Revoked token doesn't give 401 (Unautorized) error";
 
-            try
-            {
-                WebReq(sURL + validHashMd5, revokedToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("401"),
-                    $"Cought web exception, but not 401 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
-        }
-
-        [TestMethod]
-        public void IncorrectLenghtToken()
-        {
-            var invalidLengtToken = "/cprfmOkT4emoi4rvpjBBA===";
-            var errorMes = "Incorrect lenght token doesn't give 400 (Bad request) error";
-
-            try
-            {
-                WebReq(sURL + validHashMd5, invalidLengtToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("400"),
-                    $"Cought web exception, but not 400 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
-        }
-
-        [TestMethod]
-        public void IncorrectCharactersToken()
-        {
-            var invalidCharsToken = "/+3424LL34,,,,2342l";
-            var errorMes = "Incorrect chars token doesn't give 400 (Bad request) error";
-
-            try
-            {
-                WebReq(sURL + validHashMd5, invalidCharsToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("400"),
-                    $"Cought web exception, but not 400 \n {errorMes}"
-                );
-
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                 revokedToken,
+                 validHashMd5,
+                 unauthorizedtErrCode,
+                 ErrorStringBuilder(
+                    "Revoked token",
+                    unauthorizedtErrCode,
+                    unauthorizedtErrName
+                )
+             );
         }
 
         [TestMethod]
         public void UnknownToken()
         {
             var unknownToken = "LudEmW89SC6U2Nc9/1Sd7g==";
-            var errorMes = "Unknonw token doesn't give 401 (Unautorized) error";
+            
+            AssertExeptionCatcher(
+                unknownToken,
+                validHashMd5,
+                unauthorizedtErrCode,
+                ErrorStringBuilder(
+                    "Unknown token",
+                    unauthorizedtErrCode,
+                    unauthorizedtErrName
+                )
+            );
+        }
 
-            try
-            {
-                WebReq(sURL + validHashMd5, unknownToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("401"),
-                    $"Cought web exception, but not 401 \n {errorMes}"
-                );
+        [TestMethod]
+        public void IncorrectLenghtToken()
+        {
+            var invalidLengtToken = "/cprfmOkT4emoi4rvpjBBA===";
 
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
+            AssertExeptionCatcher(
+                invalidLengtToken,
+                validHashMd5,
+                badRequestErrCode,
+                ErrorStringBuilder(
+                    "Incorrect lenght token",
+                    badRequestErrCode,
+                    badRequestErrName
+                )
+            );
         }
 
         [TestMethod]
         public void EmptyStringToken()
         {
-            var emptyToken = "";
-            var errorMes = "Unknonw token doesn't give 401 (Unautorized) error";
+            var emptyStringToken = "";
 
-            try
-            {
-                WebReq(sURL + validHashMd5, emptyToken);
-            }
-            catch (WebException ex)
-            {
-                Assert.IsTrue(
-                    ex.Message.Contains("401"),
-                    $"Cought web exception, but not 401 \n {errorMes}"
-                );
-                return;
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-
-            Assert.Fail(errorMes);
-
+            AssertExeptionCatcher(
+                emptyStringToken,
+                validHashMd5,
+                badRequestErrCode,
+                ErrorStringBuilder(
+                    "Empty string token",
+                    badRequestErrCode,
+                    badRequestErrName
+                )
+            );
         }
+
+        [TestMethod]
+        public void IncorrectCharactersToken()
+        {
+            var invalidCharsToken = "/+3424LL34,,,,2342l";
+
+            AssertExeptionCatcher(
+                invalidCharsToken,
+                validHashMd5,
+                badRequestErrCode,
+                ErrorStringBuilder(
+                    "Incorrect chars token",
+                    badRequestErrCode,
+                    badRequestErrName
+                )
+            );
+        }
+        #endregion
     }
 }
 
